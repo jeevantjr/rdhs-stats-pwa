62a2a23c578357938482fe0c952e0ce81f94815b
Dr T. Jeevaraaj <trincomoinfo@gmail.com>
2025-05-28 15:39:03 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rdhs-cache-v14'; // Update this version after every significant change
+const CACHE_NAME = 'rdhs-cache-v15'; // Update this version after every significant change
 const DEBUG = false; // Set to true for development logging
 
 const urlsToCache = [
